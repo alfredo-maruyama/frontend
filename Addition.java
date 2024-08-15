6492fa5c5d968c673f090dc3232bacae08551aa4
alfredo-maruyama <alfredomaruyamafilho@gmail.com>
2024-08-15 09:21:38 -0300

    atualização e tradução addition.java

--- Addition.java
+++ Addition.java
@@ -5,16 +5,19 @@ public class Addition  //criação da classe "Addition"
    public static void main(String[] args) // criação de um método executor main
    { // declara o inicio de um bloco de códigos
       String firstNumber = // declaração de uma variável
-         JOptionPane.showInputDialog("Enter first integer"); // compotente + invocação de um método + parâmetro + mensagem que aparecerá ao usuario
+         JOptionPane.showInputDialog("Digite o primeiro número"); // compotente + invocação de um método + parâmetro + mensagem que aparecerá ao usuario
       String secondNumber = // declaração de uma variável
-         JOptionPane.showInputDialog("Enter second integer"); // compotente + invocação de um método + parâmetro + mensagem que aparecerá ao usuario
+         JOptionPane.showInputDialog("Digite o segundo número"); // compotente + invocação de um método + parâmetro + mensagem que aparecerá ao usuario
+      String thirdNumber = 
+         JOptionPane.showInputDialog("Digite o terceiro número");
 
       int number1 = Integer.parseInt(firstNumber); // forma de armazenamento da variável
       int number2 = Integer.parseInt(secondNumber); // forma de armazenamento da variável
+      int number3 = Integer.parseInt(thirdNumber);
 
-      int sum = number1 + number2; // somatória das variáveis
-      JOptionPane.showMessageDialog(null, "The sum is " + sum, // componente + invocação de um método + mensagem que aparecerá ao usuário + a somatória das variáveis
-         "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE); // mensagem que aparece ao usuário
+      int sum = number1 + number2 + number3 ; // somatória das variáveis
+      JOptionPane.showMessageDialog(null, "A soma dos números é: " + sum, // componente + invocação de um método + mensagem que aparecerá ao usuário + a somatória das variáveis
+         "Soma de três inteiros", JOptionPane.PLAIN_MESSAGE); // mensagem que aparece ao usuário
    } // declara o fim de um bloco de código
 } // declara o fim de um bloco de código
 
